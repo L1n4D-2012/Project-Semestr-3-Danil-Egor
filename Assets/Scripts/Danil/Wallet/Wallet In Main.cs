a541c08da9757d028ebef655c635ade91d8316fd
L1n4D-2012 <daniilzinchuk26@gmail.com>
2026-03-28 18:10:42 +0100

    1

--- Assets/Scripts/Danil/Wallet/Wallet In Main.cs
+++ Assets/Scripts/Danil/Wallet/Wallet In Main.cs
@@ -1,14 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WalletInMain : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
+    public TMP_Text text;
+    public string CoinsText;
+    public int coinsNow = 0;
+   
+   
 
     
+
+    private void Awake()
+    {
+        
+        coinsNow = PlayerPrefs.GetInt("CoinsMenu");
+        PlayerPrefs.Save();
+    }
+
+    public void Update_UI()
+    {
+        text.text = CoinsText;
+        CoinsText = coinsNow.ToString();
+    }
 }
--- Assets/Scripts/Egor/GameManager/GameManager.cs
+++ Assets/Scripts/Egor/GameManager/GameManager.cs
@@ -33,7 +33,7 @@ public class GameManager : MonoBehaviour
     {
         if (Wallet.instance != null)
         {
-           // Wallet.instance.ResetMoney();
+            Wallet.instance.ResetMoney();
         }
 
         if (gameOverCanvas != null)
